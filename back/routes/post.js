560c44aed900e282625686a5b59da620ea15f649
김주경 <kimjugyeong@gimjugyeong-ui-MacBookPro.local>
2021-06-20 23:13:20 +0900

    post.js images bucket name modify

--- back/routes/post.js
+++ back/routes/post.js
@@ -35,7 +35,7 @@ AWS.config.update({
 const upload = multer({
   storage: multerS3({
     s3: new AWS.S3(),
-    bucket: "react-nodebird",
+    bucket: "react-nodejoo-aws",
     key(req, file, cb) {
       cb(null, `original/${Date.now()}_${path.basename(file.originalname)}`);
     },
