5020d86bfec703f83be5c3f3ffc6c882f1608687
김주경 <kimjugyeong@gimjugyeong-ui-MacBookPro.local>
2021-06-25 21:32:52 +0900

    axios baseURL, withCredentials add

--- front/pages/profile.js
+++ front/pages/profile.js
@@ -20,6 +20,9 @@ import {
   LOAD_FOLLOWINGS_REQUEST,
 } from "../reducers/user";
 
+axios.defaults.baseURL = backUrl;
+axios.defaults.withCredentials = true;
+
 const fetcher = (url) =>
   axios.get(url, {}, { withCredentials: true }).then((result) => result.data);
 
